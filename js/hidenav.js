98bac9d3b79c05c60a8e002bf03228114628c0d5
ShrineFox <ShrineFox@users.noreply.github.com>
2021-07-07 04:12:24 -0400

    Improve navbar transparency

--- js/hidenav.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-    var scrollPos = window.pageYOffset;
-    if (scrollPos > 50) {
-        document.getElementsByClassName("navbar")[0].style.background = "linear-gradient(90deg, var(--gradient1) 0%, transparent 100%) fixed;";
-    }
-    else {
-        document.getElementsByClassName("navbar")[0].style.background = "transparent;";
-    }
-}
