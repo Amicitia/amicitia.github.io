cf2e48a73e4dbd1d66b6a548f6ee1f54cc035677
ShrineFox <ShrineFox@users.noreply.github.com>
2021-07-08 23:36:17 -0400

    Update mods

--- Program.cs
+++ Program.cs
@@ -36,11 +36,7 @@ namespace Amicitia.github.io
         {
             // Exe Directory
             indexPath = Path.GetDirectoryName(Path.GetDirectoryName(Directory.GetCurrentDirectory()));
-            // Update .tsv with data from gamebanana
-            Task.Run(async () =>
-            {
-                await Webscraper.UpdateTSVs(indexPath);
-            }).GetAwaiter().GetResult();
+
             // Order post post from .tsv files by most recent)
             posts = Post.Get(indexPath).OrderBy(p => DateTime.Parse(p.Date, CultureInfo.CreateSpecificCulture("en-US"))).ToArray().Reverse().ToList();
             // Delete files if they exist already
